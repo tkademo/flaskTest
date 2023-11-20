671c2c879e0d4145f18a977c40e769b7829b8443
tkademo <tkademo1@gmail.com>
2023-11-20 13:02:42 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+## This is a demo Flask application
+
+[Project Link] (https://www.google.com/)
+
+![Some-image](./img.webp)
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing_page():
+    # You can customize the information to be displayed on the landing page
+    company_name = "Your Company"
+    tagline = "Building a Better Future"
+    about_us = "Welcome to {}! We are dedicated to {}. Explore our website to learn more.".format(company_name, tagline)
+    
+    return render_template('landing_page.html', company_name=company_name, tagline=tagline, about_us=about_us)
+
+if __name__ == '__main__':
+    app.run(debug=True)
